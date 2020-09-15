7c99706dcd1fccbb53b35ff175867eb291babc63
Fernando dos Santos <nando4803@gmail.com>
2020-09-16 00:44:37 +0100

    🚀This commit setup light and dark themes styles

---
+++ src/assets/styles/themes/dark.ts
@@ -0,0 +1,20 @@
+import githubBackgroundNight from '../../images/background_white.svg';
+
+export default {
+  title: 'dark',
+
+  colors: {
+    ImageBackground: githubBackgroundNight,
+    primary: '#f0f0f5',
+    secondary: '#f0f0f5',
+    TitleColor: '#f0f0f5',
+    InputColor: '#3a3a3a',
+    InputPlaceholderColor: '#a8a8b3',
+    ButtonColor: '#f79622',
+    RepoStrongColor: '#3d3d4d',
+    RepoPColor: '#a8a8b3',
+    RepoPHoverColor: '#333',
+    background: '#000000',
+    text: '#333',
+  },
+};
---
+++ src/assets/styles/themes/light.ts
@@ -0,0 +1,20 @@
+import githubBackgroundDay from '../../images/githubBackgroundDay.svg';
+
+export default {
+  title: 'light',
+
+  colors: {
+    ImageBackground: githubBackgroundDay,
+    primary: '#000000',
+    secondary: '#000000',
+    TitleColor: '#3a3a3a',
+    InputColor: '#3a3a3a',
+    InputPlaceholderColor: '#a8a8b3',
+    ButtonColor: '#0000b3',
+    RepoStrongColor: '#3d3d4d',
+    RepoPColor: '#a8a8b3',
+    RepoPHoverColor: '#a8a8b3',
+    background: '#f0f0f5',
+    text: '#333',
+  },
+};
