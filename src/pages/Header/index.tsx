820fa8417420c31312feb458dd26cca27d8f0145
Fernando dos Santos <nando4803@gmail.com>
2020-09-16 11:28:59 +0100

    🚀 This commit update header component

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Switch from 'react-switch';
+import { shade } from 'polished';
+import { ThemeContext } from 'styled-components';
 
-import { Container } from './styles';
+import lightImg from '../../assets/images/logo.svg';
+import darkImg from '../../assets/images/logo-dark.svg';
 
-const Header: React.FC = () => {
-  return <div />;
+import * as S from './styles';
+
+interface Props {
+  toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+
+  let theme = '';
+  if (title === 'dark') {
+    theme = darkImg;
+  } else {
+    theme = lightImg;
+  }
+  return (
+    <S.Container>
+      <img src={theme} alt="Github Finder" />
+      <Switch
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.15, colors.primary)}
+        onColor={colors.secondary}
+      />
+    </S.Container>
+  );
 };
 
 export default Header;
