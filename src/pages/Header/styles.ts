4a78f536347f601b20c6a00a046df60bd3832efb
Fernando dos Santos <nando4803@gmail.com>
2020-09-16 11:30:37 +0100

    🚀 This commit update header styles

--- src/pages/Header/styles.ts
+++ src/pages/Header/styles.ts
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.header`
+  height: 60px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 20px;
+`;
