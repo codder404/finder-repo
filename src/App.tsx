46eb29e98dc1214bf3ca2f1a13efbcff49f16b72
Fernando dos Santos <nando4803@gmail.com>
2020-09-16 12:01:02 +0100

    🚀 This commit update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+
+import usePersisteState from './utils/usePersisteState';
+
+import Header from './pages/Header';
+
+import light from './assets/styles/themes/light';
+import dark from './assets/styles/themes/dark';
 
 import GlobalStyles from './assets/styles/globalStyles';
 import Routes from './routes';
 
-function App() {
+const App: React.FC = () => {
+  const [theme, setTheme] = usePersisteState<DefaultTheme>('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? light : dark);
+  };
+
   return (
     <>
-      <Routes />
-      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Header toggleTheme={toggleTheme} />
+
+        <Routes />
+        <GlobalStyles />
+      </ThemeProvider>
     </>
   );
-}
+};
 
 export default App;
